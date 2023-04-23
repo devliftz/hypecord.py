c98e0ab4a5dc59ddc01d1a22bd57a065ac3fdbc9
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-23 16:18:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     name='hypecord.py',
     author='nap',
     url='https://github.com/devliftz/hypecord.py',
-    version=3.6,
+    version=3.7,
     packages=packages,
     license='MIT',
     description='A Python wrapper for the Discord API',
